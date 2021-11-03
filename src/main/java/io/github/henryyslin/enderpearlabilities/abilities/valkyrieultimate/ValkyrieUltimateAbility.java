97255ae048248c012885ee1123aeb93a27c66c5d
Henry Lin <henry.ys.lin@gmail.com>
2021-11-03 17:59:50 +0800

    Working valkyrie ultimate

--- src/main/java/io/github/henryyslin/enderpearlabilities/abilities/valkyrieultimate/ValkyrieUltimateAbility.java
+++ src/main/java/io/github/henryyslin/enderpearlabilities/abilities/valkyrieultimate/ValkyrieUltimateAbility.java
@@ -13,30 +13,29 @@ import org.bukkit.*;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.RayTraceResult;
 import org.bukkit.util.Vector;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ValkyrieUltimateAbility extends Ability {
 
     static final int MAX_LAUNCH_HEIGHT = 100;
-    static final int MIN_LAUNCH_HEIGHT = 20;
+    static final int MIN_LAUNCH_HEIGHT = 10;
+    static final int INITIAL_BURN_DURATION = 30;
 
     private final AbilityInfo info;
 
     @Override
     public void setConfigDefaults(ConfigurationSection config) {
-        config.addDefault("charge-up", 0);
+        config.addDefault("charge-up", 40);
         config.addDefault("duration", 200);
         config.addDefault("cooldown", 400);
     }
@@ -66,6 +65,7 @@ public class ValkyrieUltimateAbility extends Ability {
         return info;
     }
 
+    final AtomicInteger chargeUpDuration = new AtomicInteger(0);
     final AtomicBoolean chargingUp = new AtomicBoolean(false);
     final AtomicBoolean abilityActive = new AtomicBoolean(false);
 
@@ -93,22 +93,6 @@ public class ValkyrieUltimateAbility extends Ability {
     }
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (!event.getWhoClicked().getName().equals(ownerName)) return;
-        if (event.getCurrentItem() == null) return;
-        //if (event.getSlotType() != InventoryType.SlotType.ARMOR) return;
-        if (event.getCurrentItem().getType() == Material.ELYTRA) {
-            event.setResult(Event.Result.DENY);
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    event.setCursor(new ItemStack(Material.AIR));
-                }
-            }.runTaskLater(plugin, 1);
-        }
-    }
-
-    @EventHandler
     public synchronized void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
 
@@ -120,8 +104,10 @@ public class ValkyrieUltimateAbility extends Ability {
         if (abilityActive.get()) return;
 
         if (chargingUp.get()) {
-            chargingUp.set(false);
-            abilityActive.set(true);
+            if (chargeUpDuration.get() >= info.chargeUp) {
+                chargingUp.set(false);
+                abilityActive.set(true);
+            }
         } else {
             new FunctionChain(
                     next -> new AbilityRunnable() {
@@ -130,6 +116,7 @@ public class ValkyrieUltimateAbility extends Ability {
                         @Override
                         protected void start() {
                             chargingUp.set(true);
+                            chargeUpDuration.set(0);
                             groundLocation = player.getLocation();
                         }
 
@@ -139,6 +126,7 @@ public class ValkyrieUltimateAbility extends Ability {
                                 cancel();
                                 return;
                             }
+                            chargeUpDuration.incrementAndGet();
                             boolean mainHandPearl = player.getInventory().getItemInMainHand().getType() == Material.ENDER_PEARL;
                             boolean offHandPearl = player.getInventory().getItemInOffHand().getType() == Material.ENDER_PEARL;
                             boolean shouldContinue = ability.getInfo().activation == ActivationHand.MainHand && mainHandPearl ||
@@ -147,6 +135,13 @@ public class ValkyrieUltimateAbility extends Ability {
                             if (!player.getWorld().equals(groundLocation.getWorld())) shouldContinue = false;
                             if (groundLocation.toVector().setY(0).distance(player.getLocation().toVector().setY(0)) > 1)
                                 shouldContinue = false;
+                            if (shouldContinue) {
+                                RayTraceResult result = player.getWorld().rayTraceBlocks(player.getEyeLocation(), new Vector(0, 1, 0), MIN_LAUNCH_HEIGHT, FluidCollisionMode.NEVER, true);
+                                if (result != null) {
+                                    player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "Need vertical clearance"));
+                                    shouldContinue = false;
+                                }
+                            }
                             if (shouldContinue) {
                                 if (player.getLocation().getY() < groundLocation.getY()) {
                                     groundLocation.setY(player.getLocation().getY());
@@ -161,7 +156,6 @@ public class ValkyrieUltimateAbility extends Ability {
                             } else {
                                 chargingUp.set(false);
                                 abilityActive.set(false);
-                                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(" "));
                                 cancel();
                             }
                         }
@@ -176,6 +170,8 @@ public class ValkyrieUltimateAbility extends Ability {
                     }.runTaskTimer(this, 0, 1),
                     next -> new AbilityRunnable() {
                         Location groundLocation;
+                        Location lastLocation;
+                        boolean allowGlide = false;
 
                         @Override
                         protected void start() {
@@ -189,20 +185,41 @@ public class ValkyrieUltimateAbility extends Ability {
                                 cancel();
                                 return;
                             }
-                            double propulsion = Math.min((info.duration - count) * 0.0005 + 0.08, 1.1);
-                            if (info.duration - count == 35)
+                            double propulsion;
+                            if (info.duration - count == INITIAL_BURN_DURATION) {
                                 player.setVelocity(player.getVelocity().add(new Vector(0, 1.1, 0)));
-                            if (player.getLocation().getY() - groundLocation.getY() < MAX_LAUNCH_HEIGHT) {
+                                player.getWorld().playSound(player.getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1, 0);
+                            }
+                            if (info.duration - count >= INITIAL_BURN_DURATION)
+                                propulsion = 1.1;
+                            else {
+                                propulsion = Math.min((info.duration - count) * 0.0005 + 0.075, 0.09);
+                                player.getWorld().spawnParticle(Particle.SMOKE_LARGE, player.getLocation(), 2, 0.1, 0.1, 0.1, 0.05);
+                            }
+                            boolean shouldPropel = player.getLocation().getY() - groundLocation.getY() < MAX_LAUNCH_HEIGHT;
+                            if (shouldPropel) {
+                                if (player.getLocation().equals(lastLocation)) shouldPropel = false;
+                            }
+                            if (shouldPropel) {
+                                RayTraceResult result = player.getWorld().rayTraceBlocks(player.getEyeLocation(), new Vector(0, 1, 0), 5, FluidCollisionMode.NEVER, true);
+                                if (result != null) shouldPropel = false;
+                            }
+                            if (shouldPropel) {
                                 player.setVelocity(player.getVelocity().add(new Vector(0, propulsion, 0)));
                                 player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, player.getLocation(), 2, 0.1, 0.1, 0.1, 0.05);
+                                lastLocation = player.getLocation();
                             } else {
+                                allowGlide = true;
                                 cancel();
                             }
                         }
 
                         @Override
                         protected void end() {
-                            next.run();
+                            if (allowGlide)
+                                next.run();
+                            else
+                                abilityActive.set(false);
                         }
                     }.runTaskRepeated(this, 0, 1, info.duration),
                     next -> new AbilityRunnable() {
@@ -217,6 +234,7 @@ public class ValkyrieUltimateAbility extends Ability {
                                 meta.setUnbreakable(true);
                             elytra.setItemMeta(meta);
                             elytra.addEnchantment(Enchantment.BINDING_CURSE, 1);
+                            elytra.addEnchantment(Enchantment.VANISHING_CURSE, 1);
                             player.getInventory().setChestplate(elytra);
                             player.setGliding(true);
                         }
@@ -228,12 +246,15 @@ public class ValkyrieUltimateAbility extends Ability {
                                 return;
                             }
                             RayTraceResult result = player.getWorld().rayTraceBlocks(player.getLocation(), new Vector(0, -1, 0), 1, FluidCollisionMode.ALWAYS, true);
-                            if (result != null) cancel();
+                            if (result != null) {
+                                cancel();
+                            }
                         }
 
                         @Override
                         protected void end() {
                             player.getInventory().setChestplate(chestplate);
+                            abilityActive.set(false);
                         }
                     }.runTaskTimer(this, 0, 1)
             ).execute();
