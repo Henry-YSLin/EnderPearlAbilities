de47ac860b700012057b7e9f9b68fe0c885c14de
Henry Lin <henry.ys.lin@gmail.com>
2021-12-25 10:46:15 +0800

    Improve arc wave movement

--- src/main/java/io/github/henry_yslin/enderpearlabilities/abilities/titans/ronin/RoninTitanAbility.java
+++ src/main/java/io/github/henry_yslin/enderpearlabilities/abilities/titans/ronin/RoninTitanAbility.java
@@ -4,18 +4,18 @@ import io.github.henry_yslin.enderpearlabilities.abilities.AbilityRunnable;
 import io.github.henry_yslin.enderpearlabilities.abilities.titans.TitanAbility;
 import org.bukkit.Location;
 import org.bukkit.Particle;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.IronGolem;
-import org.bukkit.entity.LivingEntity;
+import org.bukkit.Sound;
+import org.bukkit.entity.*;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 public class RoninTitanAbility extends TitanAbility<RoninTitanInfo> {
 
-    static final int ARC_WAVE_LIFETIME = 10;
-    static final int ARC_WAVE_SPEED = 2;
-    static final int ARC_WAVE_EXTRA_GRAVITY = 1;
+    static final int LIFETIME = 10;
+    static final int SPEED = 2;
+    static final int EXTRA_GRAVITY = 2;
+    static final double ANGLE_TOLERANCE = 45.0 / 180 * Math.PI;
 
     public RoninTitanAbility(Plugin plugin, RoninTitanInfo info, String ownerName) {
         super(plugin, info, ownerName);
@@ -29,14 +29,23 @@ public class RoninTitanAbility extends TitanAbility<RoninTitanInfo> {
     @Override
     public void onTitanAbilityActivate(IronGolem titan) {
         new AbilityRunnable() {
-            IronGolem arcWave;
+            Mob arcWaveLarge;
+            Mob arcWaveSmall;
             Vector velocity;
             Location lastLocation = null;
 
             @Override
             protected void start() {
-                velocity = player.getLocation().getDirection().setY(0).normalize().multiply(ARC_WAVE_SPEED);
-                arcWave = titan.getWorld().spawn(titan.getLocation(), IronGolem.class, false, entity -> {
+                velocity = player.getLocation().getDirection().setY(0).normalize().multiply(SPEED);
+                arcWaveLarge = titan.getWorld().spawn(titan.getLocation(), IronGolem.class, false, entity -> {
+                    entity.setInvulnerable(true);
+                    entity.setInvisible(true);
+                    entity.setAware(false);
+                    entity.setSilent(true);
+                    entity.setCollidable(false);
+                    entity.setVelocity(velocity);
+                });
+                arcWaveSmall = titan.getWorld().spawn(titan.getLocation(), Spider.class, false, entity -> {
                     entity.setInvulnerable(true);
                     entity.setInvisible(true);
                     entity.setAware(false);
@@ -48,34 +57,64 @@ public class RoninTitanAbility extends TitanAbility<RoninTitanInfo> {
 
             @Override
             protected void tick() {
-                if (!arcWave.isValid()) {
+                if (!arcWaveSmall.isValid() || !arcWaveLarge.isValid()) {
                     cancel();
                     return;
                 }
                 if (lastLocation != null) {
-                    if (arcWave.getLocation().distanceSquared(lastLocation) < 0.001) {
+                    Vector largeOffset = arcWaveLarge.getLocation().subtract(lastLocation).toVector().setY(0);
+                    Vector smallOffset = arcWaveSmall.getLocation().subtract(lastLocation).toVector().setY(0);
+                    Vector largeHorizontalOffset = largeOffset.clone().setY(0);
+                    Vector smallHorizontalOffset = smallOffset.clone().setY(0);
+                    double largeOffsetAngle = largeHorizontalOffset.angle(velocity.setY(0));
+                    double smallOffsetAngle = smallHorizontalOffset.angle(velocity);
+                    player.sendMessage("largeOffsetAngle: " + largeOffsetAngle / Math.PI * 180);
+                    player.sendMessage("smallOffsetAngle: " + smallOffsetAngle / Math.PI * 180);
+                    if (Math.min(largeOffsetAngle, smallOffsetAngle) > ANGLE_TOLERANCE) {
                         cancel();
                         return;
                     }
+                    if (largeOffsetAngle > ANGLE_TOLERANCE) {
+                        lastLocation = arcWaveSmall.getLocation();
+                    } else if (smallOffsetAngle > ANGLE_TOLERANCE) {
+                        lastLocation = arcWaveLarge.getLocation();
+                    } else {
+                        if (largeOffset.lengthSquared() >= smallOffset.lengthSquared()) {
+                            lastLocation = arcWaveLarge.getLocation();
+                        } else {
+                            lastLocation = arcWaveSmall.getLocation();
+                        }
+                    }
+                } else {
+                    lastLocation = arcWaveSmall.getLocation();
                 }
-                lastLocation = arcWave.getLocation();
-                for (Entity entity : arcWave.getWorld().getNearbyEntities(arcWave.getBoundingBox(),
-                        entity -> entity instanceof LivingEntity && !entity.equals(titan) && !entity.equals(player))) {
+
+                for (Entity entity : arcWaveLarge.getWorld().getNearbyEntities(arcWaveLarge.getBoundingBox(),
+                        entity -> entity instanceof LivingEntity
+                                && !entity.equals(titan)
+                                && !entity.equals(player)
+                                && !entity.equals(arcWaveSmall)
+                                && !entity.equals(arcWaveLarge))) {
                     LivingEntity livingEntity = (LivingEntity) entity;
                     livingEntity.damage(4, player);
                     livingEntity.addPotionEffect(PotionEffectType.SLOW.createEffect(60, 2));
                     livingEntity.addPotionEffect(PotionEffectType.BLINDNESS.createEffect(20, 1));
                 }
-                arcWave.setVelocity(velocity.setY(arcWave.getVelocity().getY() - ARC_WAVE_EXTRA_GRAVITY));
-                Location center = arcWave.getLocation().clone().add(0, 1.5, 0);
-                arcWave.getWorld().spawnParticle(Particle.DRAGON_BREATH, center, 20, 0.5, 0.7, 0.5, 0.01);
-                arcWave.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, arcWave.getLocation(), 10, 0.5, 0.2, 0.5, 0.01);
+
+                arcWaveLarge.setVelocity(velocity.setY(arcWaveLarge.getVelocity().getY() - EXTRA_GRAVITY));
+                arcWaveSmall.setVelocity(velocity);
+                arcWaveLarge.teleport(lastLocation);
+                arcWaveSmall.teleport(lastLocation);
+                arcWaveSmall.getWorld().spawnParticle(Particle.DRAGON_BREATH, arcWaveSmall.getLocation(), 20, 0.5, 0.7, 0.5, 0.01);
+                arcWaveSmall.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, arcWaveSmall.getLocation(), 10, 0.5, 0.2, 0.5, 0.01);
+                arcWaveSmall.getWorld().playSound(arcWaveSmall.getLocation(), Sound.ENTITY_ENDER_DRAGON_FLAP, 0.6f, 2);
             }
 
             @Override
             protected void end() {
-                arcWave.remove();
+                arcWaveLarge.remove();
+                arcWaveSmall.remove();
             }
-        }.runTaskRepeated(this, 0, 1, ARC_WAVE_LIFETIME);
+        }.runTaskRepeated(this, 0, 1, LIFETIME);
     }
 }
