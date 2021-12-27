be93b75030c4115aa36696404b319c5c4d5c4c24
Henry Lin <henry.ys.lin@gmail.com>
2021-12-27 12:13:43 +0800

    Add revenant-ultimate

---
+++ src/main/java/io/github/henry_yslin/enderpearlabilities/abilities/revenantultimate/RevenantUltimateAbility.java
@@ -0,0 +1,207 @@
+package io.github.henry_yslin.enderpearlabilities.abilities.revenantultimate;
+
+import io.github.henry_yslin.enderpearlabilities.abilities.Ability;
+import io.github.henry_yslin.enderpearlabilities.abilities.AbilityCouple;
+import io.github.henry_yslin.enderpearlabilities.abilities.AbilityRunnable;
+import io.github.henry_yslin.enderpearlabilities.utils.AbilityUtils;
+import io.github.henry_yslin.enderpearlabilities.utils.EntityUtils;
+import io.github.henry_yslin.enderpearlabilities.utils.FunctionChain;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.*;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Blaze;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class RevenantUltimateAbility extends Ability<RevenantUltimateAbilityInfo> {
+
+    public RevenantUltimateAbility(Plugin plugin, RevenantUltimateAbilityInfo info, String ownerName) {
+        super(plugin, info, ownerName);
+    }
+
+    final AtomicBoolean abilityActive = new AtomicBoolean(false);
+    final AtomicBoolean chargingUp = new AtomicBoolean(false);
+    AtomicReference<Blaze> totem = new AtomicReference<>();
+
+    @Override
+    public boolean isActive() {
+        return abilityActive.get();
+    }
+
+    @Override
+    public boolean isChargingUp() {
+        return chargingUp.get();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        if (player != null) {
+            abilityActive.set(false);
+            cooldown.setCooldown(info.getCooldown());
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        super.onPlayerJoin(event);
+        Player player = event.getPlayer();
+        if (player.getName().equals(ownerName)) {
+            abilityActive.set(false);
+            cooldown.setCooldown(info.getCooldown());
+        }
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (!AbilityUtils.verifyAbilityCouple(this, event.getEntity())) return;
+        event.getDrops().clear();
+        event.setDroppedExp(0);
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+        if (!player.getName().equals(ownerName)) return;
+        if (!abilityActive.get()) return;
+        Blaze blaze = totem.get();
+        if (blaze == null || !blaze.isValid()) return;
+        if (player.getHealth() - event.getFinalDamage() <= 0) {
+            event.setCancelled(true);
+            player.setHealth(EntityUtils.getMaxHealth(player) / 2);
+            player.teleport(blaze.getLocation());
+            Bukkit.broadcastMessage(player.getName() + " was protected from death");
+            blaze.remove();
+
+            player.getWorld().playSound(player.getLocation(), Sound.ENTITY_ELDER_GUARDIAN_CURSE, 1, 2);
+            blaze.getWorld().spawnParticle(Particle.SOUL_FIRE_FLAME, blaze.getEyeLocation(), 20, 0.5, 0.5, 0.5, 0);
+        }
+    }
+
+    @EventHandler
+    public synchronized void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+
+        if (!AbilityUtils.abilityShouldActivate(event, ownerName, info.getActivation())) return;
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+        if (event.getClickedBlock() == null) return;
+
+        event.setCancelled(true);
+
+        if (cooldown.isCoolingDown()) return;
+        if (chargingUp.get()) return;
+        if (abilityActive.get()) return;
+
+        World world = player.getWorld();
+
+        if (!event.getClickedBlock().getType().isSolid()) return;
+        Location location = event.getClickedBlock().getLocation();
+        location.add(0.5, 1, 0.5);
+        if (location.getBlock().getType() != Material.AIR) return;
+
+        AbilityUtils.consumeEnderPearl(this, player);
+
+        new FunctionChain(
+                next -> {
+                    if (totem.get() != null && totem.get().isValid()) {
+                        totem.get().remove();
+                    }
+                    Blaze blaze = world.spawn(location, Blaze.class, false, entity -> {
+                        entity.setGravity(false);
+                        entity.setGlowing(true);
+                        entity.setAI(false);
+                        entity.setMetadata("ability", new FixedMetadataValue(plugin, new AbilityCouple(info.getCodeName(), ownerName)));
+                    });
+                    totem.set(blaze);
+
+                    totem.get().getWorld().playSound(player.getLocation(), Sound.ENTITY_BLAZE_HURT, 1, 0);
+
+                    next.run();
+                },
+                next -> new AbilityRunnable() {
+                    BossBar bossBar;
+
+                    @Override
+                    protected void start() {
+                        bossBar = Bukkit.createBossBar("Charging up", BarColor.WHITE, BarStyle.SOLID);
+                        bossBar.addPlayer(player);
+                        chargingUp.set(true);
+                    }
+
+                    @Override
+                    protected void tick() {
+                        if (totem.get() == null || !totem.get().isValid() || !player.isValid()) {
+                            cancel();
+                            return;
+                        }
+                        bossBar.setProgress(1 - count / (float) info.getChargeUp() * 2);
+                        world.spawnParticle(Particle.FIREWORKS_SPARK, totem.get().getEyeLocation(), 4, 0.1, 0.1, 0.1, 0.02);
+                    }
+
+                    @Override
+                    protected void end() {
+                        bossBar.removeAll();
+                        chargingUp.set(false);
+                        if (this.hasCompleted())
+                            next.run();
+                        else {
+                            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "Totem destroyed"));
+                            totem.get().remove();
+                            cooldown.setCooldown(5 * 20);
+                        }
+                    }
+                }.runTaskRepeated(this, 0, 2, info.getChargeUp() / 2),
+                next -> new AbilityRunnable() {
+                    BossBar bossBar;
+
+                    @Override
+                    protected void start() {
+                        bossBar = Bukkit.createBossBar(ChatColor.LIGHT_PURPLE + info.getName(), BarColor.PURPLE, BarStyle.SOLID);
+                        bossBar.addPlayer(player);
+                        abilityActive.set(true);
+                        totem.get().getWorld().playSound(player.getLocation(), Sound.ENTITY_ELDER_GUARDIAN_CURSE, 1, 0);
+                        totem.get().getWorld().spawnParticle(Particle.SOUL, totem.get().getEyeLocation(), 20, 0.5, 0.5, 0.5, 0.02);
+                    }
+
+                    @Override
+                    protected void tick() {
+                        if (totem.get() == null || !totem.get().isValid()) {
+                            cancel();
+                            return;
+                        }
+                        bossBar.setProgress(count / (float) info.getDuration() * 5);
+                        totem.get().getWorld().spawnParticle(Particle.SOUL, totem.get().getEyeLocation(), 1, 0.1, 0.1, 0.1, 0.02);
+                    }
+
+                    @Override
+                    protected void end() {
+                        bossBar.removeAll();
+                        if (!hasCompleted())
+                            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.RED + "Totem destroyed"));
+                        abilityActive.set(false);
+                        totem.get().remove();
+                        totem.set(null);
+                        cooldown.setCooldown(info.getCooldown());
+                    }
+                }.runTaskRepeated(this, 0, 5, info.getDuration() / 5)
+        ).execute();
+    }
+}
---
+++ src/main/java/io/github/henry_yslin/enderpearlabilities/abilities/revenantultimate/RevenantUltimateAbilityInfo.java
@@ -0,0 +1,57 @@
+package io.github.henry_yslin.enderpearlabilities.abilities.revenantultimate;
+
+import io.github.henry_yslin.enderpearlabilities.Instantiable;
+import io.github.henry_yslin.enderpearlabilities.abilities.AbilityWithDurationInfo;
+import io.github.henry_yslin.enderpearlabilities.abilities.ActivationHand;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.Plugin;
+
+@Instantiable
+public class RevenantUltimateAbilityInfo extends AbilityWithDurationInfo {
+
+    @Override
+    public void writeConfigDefaults(ConfigurationSection config) {
+        config.addDefault("charge-up", 2 * 20);
+        config.addDefault("duration", 60 * 20);
+        config.addDefault("cooldown", 180 * 20);
+    }
+
+    @Override
+    public String getCodeName() {
+        return "revenant-ultimate";
+    }
+
+    @Override
+    public String getName() {
+        return "Death Totem";
+    }
+
+    @Override
+    public String getOrigin() {
+        return "Apex Legends - Revenant";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Drop a totem that protects the user from death. Instead of getting killed, you will return to the totem.";
+    }
+
+    @Override
+    public String getUsage() {
+        return "Right click with an ender pearl to place the death totem. When you die while the totem is active, you will return to the totem.";
+    }
+
+    @Override
+    public ActivationHand getActivation() {
+        return ActivationHand.MainHand;
+    }
+
+    @Override
+    public RevenantUltimateAbility createInstance(String ownerName) {
+        return new RevenantUltimateAbility(plugin, this, ownerName);
+    }
+
+    public RevenantUltimateAbilityInfo(Plugin plugin) {
+        super(plugin);
+    }
+}
