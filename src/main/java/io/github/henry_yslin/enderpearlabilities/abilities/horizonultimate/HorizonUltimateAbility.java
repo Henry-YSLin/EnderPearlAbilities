2d3d82a7ccddbd585b50841e1b089dfff2b05499
Henry Lin <henry.ys.lin@gmail.com>
2022-06-17 20:42:07 +0800

    Change horizon ultimate to have a physical object

--- src/main/java/io/github/henry_yslin/enderpearlabilities/abilities/horizonultimate/HorizonUltimateAbility.java
+++ src/main/java/io/github/henry_yslin/enderpearlabilities/abilities/horizonultimate/HorizonUltimateAbility.java
@@ -2,6 +2,7 @@ package io.github.henry_yslin.enderpearlabilities.abilities.horizonultimate;
 
 import io.github.henry_yslin.enderpearlabilities.EnderPearlAbilities;
 import io.github.henry_yslin.enderpearlabilities.abilities.Ability;
+import io.github.henry_yslin.enderpearlabilities.abilities.AbilityCouple;
 import io.github.henry_yslin.enderpearlabilities.abilities.AbilityRunnable;
 import io.github.henry_yslin.enderpearlabilities.events.AbilityActivateEvent;
 import io.github.henry_yslin.enderpearlabilities.events.EventListener;
@@ -10,14 +11,13 @@ import org.bukkit.*;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.boss.BossBar;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.Snowball;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.projectiles.ProjectileSource;
 import org.bukkit.util.Vector;
@@ -86,6 +86,12 @@ public class HorizonUltimateAbility extends Ability<HorizonUltimateAbilityInfo> 
     }
 
     @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (!AbilityUtils.verifyAbilityCouple(this, event.getEntity())) return;
+        event.getDrops().clear();
+    }
+
+    @EventHandler
     public void onProjectileHit(ProjectileHitEvent event) {
         Projectile projectile = event.getEntity();
         ProjectileSource shooter = projectile.getShooter();
@@ -106,10 +112,18 @@ public class HorizonUltimateAbility extends Ability<HorizonUltimateAbilityInfo> 
         );
 
         projectile.remove();
-        abilityActive.set(true);
         blockShoot.set(false);
 
         Location finalLocation = ProjectileUtils.correctProjectileHitLocation(projectile);
+
+        Shulker device = projectile.getWorld().spawn(finalLocation, Shulker.class, entity -> {
+            entity.setAI(false);
+            entity.setSilent(true);
+            entity.setRemoveWhenFarAway(false);
+            entity.setPeek(0);
+            entity.setColor(DyeColor.PURPLE);
+            entity.setMetadata("ability", new FixedMetadataValue(plugin, new AbilityCouple(info.getCodeName(), ownerName)));
+        });
 
         WorldUtils.spawnParticleCubeOutline(finalLocation.clone().add(-5.5, -5.5, -5.5), finalLocation.clone().add(5.5, 5.5, 5.5), Particle.VILLAGER_HAPPY, 5, true);
 
@@ -124,37 +138,48 @@ public class HorizonUltimateAbility extends Ability<HorizonUltimateAbilityInfo> 
 
                     @Override
                     protected void tick() {
-                        world.playSound(finalLocation, Sound.ENTITY_BLAZE_AMBIENT, 1, 0);
+                        if (!device.isValid()) {
+                            cancel();
+                            return;
+                        }
+                        device.setPeek(1f - count / (float) info.getChargeUp() * 2);
+                        world.playSound(device.getLocation(), Sound.ENTITY_BLAZE_AMBIENT, 1, 0);
                         world.spawnParticle(Particle.EXPLOSION_NORMAL, finalLocation, 2, 0.5, 0.5, 0.5, 0.02);
                     }
 
                     @Override
                     protected void end() {
                         chargingUp.set(false);
+                        device.setPeek(1);
                         if (this.hasCompleted())
                             nextFunction.run();
                     }
                 }.runTaskRepeated(this, 0, 2, info.getChargeUp() / 2),
                 nextFunction -> new AbilityRunnable() {
-                    Location blackHoleLocation;
                     BossBar bossBar;
 
                     @Override
                     protected void start() {
-                        blackHoleLocation = finalLocation.clone();
-                        world.playSound(blackHoleLocation, Sound.BLOCK_END_PORTAL_SPAWN, 1, 0);
+                        abilityActive.set(true);
+                        world.playSound(device.getLocation(), Sound.BLOCK_END_PORTAL_SPAWN, 1, 0);
                         bossBar = Bukkit.createBossBar(ChatColor.LIGHT_PURPLE + info.getName(), BarColor.PURPLE, BarStyle.SOLID);
                         bossBar.addPlayer(player);
                     }
 
                     @Override
                     protected void tick() {
+                        if (!device.isValid()) {
+                            cancel();
+                            return;
+                        }
+                        Location deviceLocation = device.getLocation();
                         bossBar.setProgress((double) count / info.getDuration());
-                        world.getNearbyEntities(blackHoleLocation, 5.5, 5.5, 5.5).forEach(entity -> {
+                        world.getNearbyEntities(deviceLocation, 5.5, 5.5, 5.5).forEach(entity -> {
+                            if (entity == device) return;
                             if (entity instanceof Player player && player.getGameMode() == GameMode.SPECTATOR) return;
                             Vector velocity = MathUtils.clamp(entity.getVelocity(), 0.8);
                             velocity = MathUtils.replaceInfinite(velocity, new Vector(0, 0, 0));
-                            Vector blackHole = blackHoleLocation.toVector().subtract(entity.getLocation().toVector());
+                            Vector blackHole = deviceLocation.toVector().subtract(entity.getLocation().toVector());
                             double distance = blackHole.length();
                             blackHole.normalize().multiply(Math.min(1, 1.25 / distance));
                             blackHole = MathUtils.replaceInfinite(blackHole, new Vector(0, 0, 0));
@@ -163,18 +188,17 @@ public class HorizonUltimateAbility extends Ability<HorizonUltimateAbilityInfo> 
                                 if (entity instanceof LivingEntity livingEntity)
                                     livingEntity.damage(1, player);
                         });
-                        //blackHoleLocation.add(0, 0.05, 0);
-                        world.spawnParticle(Particle.SMOKE_LARGE, blackHoleLocation, 5, 0.5, 0.5, 0.5, 0.2);
+                        world.spawnParticle(Particle.SMOKE_LARGE, deviceLocation, 5, 0.5, 0.5, 0.5, 0.2);
                         if (count % 20 == 19) {
-                            WorldUtils.spawnParticleCubeOutline(blackHoleLocation.clone().add(-5.5, -5.5, -5.5), blackHoleLocation.clone().add(5.5, 5.5, 5.5), Particle.END_ROD, 5, true);
+                            WorldUtils.spawnParticleCubeOutline(deviceLocation.clone().add(-5.5, -5.5, -5.5), deviceLocation.clone().add(5.5, 5.5, 5.5), Particle.END_ROD, 5, true);
                         }
                     }
 
                     @Override
                     protected void end() {
                         bossBar.removeAll();
-                        if (this.hasCompleted())
-                            cooldown.setCooldown(info.getCooldown());
+                        device.remove();
+                        cooldown.setCooldown(info.getCooldown());
                         abilityActive.set(false);
                         nextFunction.run();
                     }
